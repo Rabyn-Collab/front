df16a8f2a2aa1d4fa47535fe4b82c771d6c6b84b
Rabyn900 <babynshrestha64@gmail.com>
2023-02-22 08:05:16 +0545

    changes

--- src/features/blog/blogApi.js
+++ src/features/blog/blogApi.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const blogApi = createApi({
   reducerPath: 'blogApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://blogs-some.onrender.com' }),
   tagTypes: ['Blogs'],
   endpoints: (builder) => ({
 
